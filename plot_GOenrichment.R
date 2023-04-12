355c09321717639e3694f6e2786dc479a0406639
Sofía Salazar <sogusama02@gmail.com>
2023-04-11 19:47:38 -0600

    scripts for figures

---
+++ plot_GOenrichment.R
@@ -0,0 +1,147 @@
+
+# GO enrichment
+#####
+
+library(gprofiler2)
+library(enrichplot)
+library(DOSE)
+library(clusterProfiler)
+library(ggplot2)
+
+load("/mnt/Citosina/amedina/ssalazar/meta/combined/namedDGElist.RData")
+
+outdir = "/mnt/Citosina/amedina/ssalazar/meta/combined/figures/"
+
+#####
+
+up <- data %>%filter(Expression == 'Up-regulated') %>% 
+  arrange(padj, desc(abs(log2FoldChange)))
+
+down <- data %>%filter(Expression == 'Down-regulated') %>% 
+  arrange(padj, desc(abs(log2FoldChange)))
+
+# query
+
+multi_gp <- gost(list("Upregulated" = up$gene_name, "Downregulated" = down$gene_name), 
+                 correction_method = "fdr", multi_query = F, ordered_query = T, 
+                 organism = 'hsapiens')
+
+# manhattan plot
+
+gostp1 <- gostplot(multi_gp, interactive = FALSE)
+ggsave(paste0(outdir,"manhattanGO.png"),
+       plot = gostp1, dpi = 300)
+dev.off()
+
+#####
+
+# barplots
+
+bar_data <- data.frame("term" = as.factor(gost_query$term_name), "condition" = gost_query$query, "count" = gost_query$term_size, "p.adjust" = gost_query$p_value, 'category' = as.factor(gost_query$source))
+
+
+bar_data_up <- subset(bar_data, condition == 'Upregulated')
+bar_data_up <- head(bar_data_up[order(bar_data_up$p.adjust),],15)
+
+bar_data_down <- subset(bar_data, condition == 'Downregulated')
+bar_data_down <- head(bar_data_down[order(bar_data_down$p.adjust),],15)
+
+bar_data_reduced <- rbind(bar_data_up, bar_data_down)
+
+bar_data_ordered <- bar_data_reduced[order(bar_data_reduced$count),] # order by count
+bar_data_ordered<- bar_data_ordered[order(bar_data_ordered$category),] # order by category
+bar_data_ordered$num <- seq(1:nrow(bar_data_ordered)) # num category for plot
+
+g <- ggplot(bar_data_ordered, aes(count, reorder(term, -num), fill = category)) +
+  geom_bar(stat = "identity") +
+  geom_text(
+    aes(label = count),
+    color = "black",
+    hjust = -0.1,
+    size = 4,
+    position = position_dodge(0.9)
+  ) +
+  labs(x = "Gene counts" ) +
+  scale_fill_manual(name='Category', labels = c('Biological Process', 'Cellular Component',
+                                                'Molecular Function', 'REAC'), values = c('#3C6997', '#DD7230','#B4DC7F','#25ced1')) +
+  theme(
+    legend.position = "right",
+    # panel.grid = element_blank(),
+    # axis.text.x = element_blank(),
+    # axis.ticks = element_blank(),
+    axis.title.y = element_blank(),
+    strip.text.x = element_text(size = 14, face = "bold"),
+    strip.background = element_blank()
+  )
+
+ggsave(paste0(outdir,"barplotGO.png"),
+       plot = g, dpi = 300, width = 20, height = 10)
+dev.off()
+
+## barplot only upregulated
+
+bar_data_up <- subset(bar_data, condition == 'Upregulated')
+bar_data_up <-head(bar_data_up[order(bar_data_up$p.adjust),],40) # order by pvalue
+bar_data_up_ordered <- bar_data_up[order(bar_data_up$count),] # order by count
+bar_data_up_ordered<- bar_data_up_ordered[order(bar_data_up_ordered$category),] # order by category
+bar_data_up_ordered$num <- seq(1:nrow(bar_data_up_ordered)) # num category for plot
+
+g.up <- ggplot(bar_data_up_ordered, aes(count, reorder(term, -num), fill = category)) +
+  geom_bar(stat = "identity") +
+  geom_text(
+    aes(label = count),
+    color = "black",
+    hjust = -0.1,
+    size = 4,
+    position = position_dodge(0.9)
+  ) +
+  labs(x = "Gene counts" ) +
+  scale_fill_manual(name='Category', labels = c('Biological Process', 'REAC', 'TF'), values = c('#3C6997', '#DD7230','#B4DC7F','#25ced1')) +
+  theme(
+    legend.position = "right",
+    # panel.grid = element_blank(),
+    # axis.text.x = element_blank(),
+    # axis.ticks = element_blank(),
+    axis.title.y = element_blank(),
+    strip.text.x = element_text(size = 14, face = "bold"),
+    strip.background = element_blank()
+  )
+
+ggsave(paste0(outdir,"barplotUP_GO.png"),
+       plot = g.up, dpi = 300, width = 20, height = 10)
+dev.off()
+
+## barplot only downregulated
+
+bar_data_down <- subset(bar_data, condition == 'Downregulated')
+bar_data_down <-head(bar_data_down[order(bar_data_down$p.adjust),],40) # order by pvalue
+bar_data_down_ordered <- bar_data_down[order(bar_data_down$count),] # order by count
+bar_data_down_ordered<- bar_data_down_ordered[order(bar_data_down_ordered$category),] # order by category
+bar_data_down_ordered$num <- seq(1:nrow(bar_data_down_ordered)) # num category for plot
+
+g.down <- ggplot(bar_data_down_ordered, aes(count, reorder(term, -num), fill = category)) +
+  geom_bar(stat = "identity") +
+  geom_text(
+    aes(label = count),
+    color = "black",
+    hjust = -0.1,
+    size = 4,
+    position = position_dodge(0.9)
+  ) +
+  labs(x = "Gene counts" ) +
+  scale_fill_manual(name='Category', labels = c('CORUM', 'Biological Process', 'Cellular Component', 'Molecular Function'), values = c('#e810cb', '#3C6997','#d9c621','#b30039')) +
+  theme(
+    legend.position = "right",
+    # panel.grid = element_blank(),
+    # axis.text.x = element_blank(),
+    # axis.ticks = element_blank(),
+    axis.title.y = element_blank(),
+    strip.text.x = element_text(size = 14, face = "bold"),
+    strip.background = element_blank()
+  )
+
+ggsave(paste0(outdir,"barplotDOWN_GO.png"),
+       plot = g.down, dpi = 300, width = 20, height = 10)
+dev.off()
+
+#####
---
+++ plot_ViolinPlots.R
@@ -0,0 +1,97 @@
+# Violin Plots
+#####
+library(ggplot2)
+library(DESeq2)
+library(tidyverse)
+library(perm)
+load('/mnt/Citosina/amedina/ssalazar/meta/out/LRT-dds.RData')
+load("/mnt/Citosina/amedina/ssalazar/meta/combined/vsd2.RData")
+load("/mnt/Citosina/amedina/ssalazar/meta/combined/namedDGElist.RData")
+outdir = '/Users/sofiasalazar/Desktop/LAB/meta-analysis-SLE/combined/figures/'
+#####
+
+#####
+# In cluster
+
+head(df_names)
+
+# name norm matrix with gene name
+norm_counts <- as.data.frame(assay(vsd2))
+norm_counts <- tibble::rownames_to_column(norm_counts, "ID")
+name_short<-function(table){
+  table$ID <- gsub("\\..*","", table$ID)
+  return(table)
+}
+
+norm_counts_id <- name_short(norm_counts)
+
+norm_counts_name <- inner_join(norm_counts_id, df_names, by = 'ID')
+
+norm_counts_name<- norm_counts_name[,-c(1,320:327)]
+rownames(norm_counts_name) <- df_names$gene_name
+
+write.csv(norm_counts_name, "/mnt/Citosina/amedina/ssalazar/meta/combined/normcounts_name.csv")
+
+###### 
+
+## In local
+
+# Boxplots
+norm_counts_name <- read.csv('/Users/sofiasalazar/Desktop/LAB/meta-analysis-SLE/combined/normcounts_name.csv')
+all_data <-  read.csv('/Users/sofiasalazar/Desktop/LAB/meta-analysis-SLE/combined/all_data.csv')
+
+rownames(norm_counts_name) <- norm_counts_name$X
+norm_counts_name<-norm_counts_name[,-c(1)]
+
+list.genes <- c("IFI27","BANK1","BLK","C1QA","C1QB","C1QC","C2","FCGR2A","FCGR3A","IRF5","ITGAM","LYN","STAT4","SLC1A7","TREX1","DNASE1L3","TLR7","TLR9","IFI44L","IL12RB2",'HLA-A','HLA-B','HLA-C')
+
+for (i in 1:length(list.genes)){
+  gene <- list.genes[i]
+  counts.gene <- norm_counts_name[rownames(norm_counts_name)==gene,]
+  counts.gene<- as.data.frame(t(counts.gene))
+  
+  # test
+  expression = counts.gene[,1]
+  sample = all_data$DISEASE
+  df.gene <- data.frame(expression, sample)
+  
+  control.e <- df.gene[df.gene$sample=='CONTROL',]$expression
+  sle.e<- df.gene[df.gene$sample=='SLE',]$expression
+  test <- permTS(control.e, sle.e)
+  
+  # wilcox.test(x = control.e, y = sle.e, alternative = "two.sided", mu = 0,
+  # paired = FALSE, conf.int = 0.95)
+  if(test$p.value < 0.05){
+    print(gene)
+    # plot
+    
+    p <- ggplot(df.gene, aes(x=sample, y=expression, fill=sample)) + 
+      geom_violin(trim=FALSE) +
+      labs(title=gene,x = NULL, y="Normalized counts") +
+      ylim(0,18)
+    
+    p1 <- p + geom_boxplot(width=0.05, color = 'black', fill='gray') +
+      # geom_jitter(shape=16, position=position_jitter(0.2)) +
+      scale_fill_manual(values=c("#e65a5a", "#4d80c4")) +
+      stat_summary(fun.y=mean, geom = 'point', shape = 23, size = 2) +
+      theme_classic()
+    
+    p2 <- p1 + theme(legend.position="none") +
+      annotate("text",
+               x = 1:length(table(df.gene$sample)),
+               y = 17,
+               label = paste0('n = ', table(df.gene$sample)),
+               col = "black",
+               vjust = - 1)
+    
+    p3 <- p2 + annotate("text",
+                        x = 1,
+                        y = 15,
+                        label = paste0('Perm test, p = ', signif(test$p.value, digits = 4)),
+                        col = "black",
+                        vjust = - 1)
+    
+    ggsave(paste0("/Users/sofiasalazar/Desktop/LAB/meta-analysis-SLE/combined/figures/violinplot",gene,"-ptest.png"), dpi = 300, plot = p3)  
+    
+  }
+}
